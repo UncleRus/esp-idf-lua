41656434c488bd564e02d61ffad774368d52d14f
UncleRus <unclerus@gmail.com>
2019-09-27 12:33:47 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Lua component for ESP-IDF
+
+https://www.lua.org
+
+Version 5.3.5
+
+## What is Lua?
+
+Lua is a powerful, efficient, lightweight, embeddable scripting language. 
+It supports procedural programming, object-oriented programming, functional
+programming, data-driven programming, and data description.
+
+Lua combines simple procedural syntax with powerful data description
+constructs based on associative arrays and extensible semantics. Lua is
+dynamically typed, runs by interpreting bytecode with a register-based
+virtual machine, and has automatic memory management with incremental
+garbage collection, making it ideal for configuration, scripting, and
+rapid prototyping. 
+
+## How to use
+
+Clone this repository into `components` subdir of yout project:
+
+```Shell
+cd ~/my_esp_idf_project
+mkdir components
+cd components
+git clone git@github.com:UncleRus/esp-idf-lua.git
+```
+
+Or clone it to any other directory and add it to your project `Makefile` or `CMakeLists.txt`:
+
+```Makefile
+PROJECT_NAME := my-esp-project
+EXTRA_COMPONENT_DIRS := $(HOME)/my/work/path/esp-idf-lua
+include $(IDF_PATH)/make/project.mk
+```
+
+```cmake
+cmake_minimum_required(VERSION 3.5)
+set(EXTRA_COMPONENT_DIRS $ENV{HOME}/my/work/path/esp-idf-lua)
+include($ENV{IDF_PATH}/tools/cmake/project.cmake)
+project(my-esp-project)
+```
+
+See example.
---
+++ example/main/main.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <string.h>
+#include <lua/lua.h>
+#include <lua/lauxlib.h>
+#include <lua/lualib.h>
+
+static const char *prg =
+        "v = {'value1', 'value2', 1.21, 'gigawatts'}\n"
+        "for i = 1, #v do\n"
+        "  print(v[i])\n"
+        "end\n";
+
+static int report(lua_State *L, int status)
+{
+    if (status != LUA_OK)
+    {
+        const char *msg = lua_tostring(L, -1);
+        lua_writestring(msg, strlen(msg));
+        lua_writeline();
+        lua_pop(L, 1); /* remove message */
+    }
+    return status;
+}
+
+void test(void *arg)
+{
+    printf("Start, heap: %d\n", xPortGetFreeHeapSize());
+
+    lua_State *L = luaL_newstate();
+    if (!L)
+    {
+        printf("Could not create state....\n");
+        while (1) { vTaskDelay(1000); }
+    }
+
+    printf("State ready, heap: %d\n", xPortGetFreeHeapSize());
+
+    luaL_openlibs(L);
+
+    printf("Libs ready, heap: %d\n", xPortGetFreeHeapSize());
+
+    int r = luaL_loadstring(L, prg);
+    if (r)
+    {
+        report(L, r);
+        while (1) { vTaskDelay(1000); }
+    }
+
+    printf("Prg loaded, heap: %d\n", xPortGetFreeHeapSize());
+
+    r = lua_pcall(L, 0, 0, 0);
+    if (r)
+        report(L, r);
+
+    printf("Prg done, heap: %d\n", xPortGetFreeHeapSize());
+
+    lua_close(L);
+
+    printf("State closed, heap: %d\n", xPortGetFreeHeapSize());
+    while (1)
+    {
+        printf(".");
+        vTaskDelay(100);
+    }
+}
+
+void app_main()
+{
+    xTaskCreate(test, "test", 0x10000, NULL, 5, NULL);
+}
